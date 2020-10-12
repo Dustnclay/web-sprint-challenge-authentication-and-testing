c8330a51d628e9110ed08391630b1a27a3c8145d
dustin <dustnclaytiner@gmail.com>
2020-10-11 20:48:26 -0500

    login done

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const bcryptjs = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const Jokes = require('../jokes/jokes-model')
-const jwtSecret = require('../auth/jwtSecret')
+const jwtSecret = 'secretsss'
 
 router.post('/register', (req, res) => {
   const user = req.body
@@ -22,6 +22,43 @@ router.post('/register', (req, res) => {
 
 router.post('/login', (req, res) => {
   // implement login
+  
+  let {username,password} = req.body
+  // console.log('username', username,'username', {username})
+  Jokes.findBy({username})
+  .first()
+  .then((user) => {
+    console.log('userin the .then:',user)
+    if( user && bcryptjs.compareSync(password,user.password)){
+      console.log('madeitdown herre1')
+      const token = generateToken(user);
+      console.log('madeitdown herre2')
+      res.status(200).json({
+        message:`welcome ${user.username}`,
+        token
+      })
+    }else{
+      res.status(401).json({message:'incalid creds'})
+    }
+  }).catch((err) => {
+    res.status(500).json(err)
+  })
 });
+
+function generateToken(user) {
+  console.log('made it to the tokengenerater')
+  const payload = {
+    
+    subject: user.id,
+    username: user.username,
+    time:Date.now()
+  }
+  console.log('made it to the tokengenerater1')
+  const options = {
+    expiresIn: '1h'
+  }
+  console.log('made it to the tokengenerater2')
+  return jwt.sign(payload,jwtSecret,options)
+}
 
 module.exports = router;
